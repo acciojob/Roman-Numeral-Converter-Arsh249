ed2d5c430102ba75a9d6daf0bbb2e7e378ef281c
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-04-07 14:20:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+  obj.reduce((acc,[k,v]) => {
+	  acc+=k.repeat(Math.floor(num/v));
+	  num=num%v;
+	  return acc;
+  },'');
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
